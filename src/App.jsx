f112da50400e2043185a054a2ad6261da94af523
aarekr <aare212@hotmail.com>
2024-09-03 17:45:58 +0300

    User can add priority and number to new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,36 @@ import { useState } from "react"
 const App = () => {
   const [ tasks, setTasks ] = useState(
     [
-      { letter: 'A', number: 1, task: 'Java 1 kpl' },
-      { letter: 'A', number: 2, task: 'C# osa 1'},
-      { letter: 'B', number: 1, task: 'Lue 50 sivua'}
+      { priority: 'A', number: 1, name: 'Java 1 kpl' },
+      { priority: 'A', number: 2, name: 'C# osa 1'},
+      { priority: 'B', number: 1, name: 'Lue 50 sivua'}
     ]
   )
+  const [ priority, setPriority] = useState('')
+  const [ number, setNumber ] = useState('')
   const [ newTask, setNewTask ] = useState('')
   const handleTaskChange = (event) => {
     setNewTask(event.target.value)
   }
+  const handlePriorityChange = (event) => {
+    setPriority(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value)
+  }
   const addNewTask = (event) => {
+    console.log("addNewTask priority: ", priority)
+    console.log("addNewTask number  : ", number)
+    console.log("addNewTask newTask : ", newTask)
     event.preventDefault()
     const taskObject = {
-      letter: '_',
-      number: '_',
-      task: newTask,
+      priority: priority,
+      number: number,
+      name: newTask,
     }
     setTasks(tasks.concat(taskObject))
+    setPriority('')
+    setNumber('')
     setNewTask('')
   }
 
@@ -29,10 +42,15 @@ const App = () => {
       <hr/>
       <h3>Prioritized Daily Task List</h3>
       <ul>
-        {tasks.map(item => <li>{item.letter}{item.number} : {item.task}</li>)}
+        {tasks.map(item => <li key={item.name}>{item.priority}{item.number} : {item.name}</li>)}
       </ul>
+      <hr />
+      <h4>Add a new task</h4>
       <form onSubmit={addNewTask}>
-        <input value={newTask} onChange={handleTaskChange} />
+        Task: <input value={newTask} onChange={handleTaskChange} /> <br />
+        Priority: <input value={priority} onChange={handlePriorityChange} /> <br />
+        Number: <input value={number} onChange={handleNumberChange} /> <br />
+        <br />
         <button type='submit'>Add new task</button>
       </form>
     </div>
