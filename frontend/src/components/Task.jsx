8409d1d6e933acf58704cf0bcc9cff7d3b98f6ea
aarekr <aare212@hotmail.com>
2024-09-10 12:54:15 +0300

    Frontend Vitest setup, 1 test

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,12 +1,12 @@
 const Task = ({ setToDone, id, done, priority, number, name, text }) => {
 
    return (
-    <tr>
+    <tr className="task">
         <td width="100" align="center"><button onClick={() => setToDone(id)}>{text}</button></td>
         <td width="50" align="center">{done}</td>
         <td width="30" align="center">{priority}</td>
         <td width="30" align="center">{number}</td>
-        <td width="250">{name}</td>
+        <td width="250" className="task">{name}</td>
     </tr>
    )
 }
--- frontend/src/components/Task.test.jsx
+++ frontend/src/components/Task.test.jsx
@@ -1,16 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import Task from './Task'
+import { expect } from 'vitest'
 
-test('renders name', () => {
+test('renders name, priority, number, done correctly', async () => {
   const task = {
     done: 'false',
     priority: 'A',
     number: 1,
-    name: 'Java',
+    name: 'Java Spring Boot Advanced Security',
   }
 
-  render(<Task task={task} />)
+  render(<Task done={task.done} priority={task.priority} number={task.number}
+    name={task.name} text={'mark done'} />)
+  
+  // screen.debug()             // shows HTML in console
+  // screen.debug(elementName)  // shows element in console
 
-  const element = screen.getByText('Java')
-  expect(element).toBeDefined()
+  const elementName = screen.getByText('Java Spring Boot Advanced Security')
+  expect(elementName).toBeDefined()
+  const elementPriority = screen.getByText('A')
+  expect(elementPriority).toBeDefined()
+  const elementNumber = screen.getByText('1')
+  expect(elementNumber).toBeDefined()
+  const elementDone = screen.getByText('false')
+  expect(elementDone).toBeDefined()
 })
