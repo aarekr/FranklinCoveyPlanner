4055f547393214132e879421b07fac2a1d7ff2f5
aarekr <aare212@hotmail.com>
2024-09-23 10:56:10 +0300

    Navigation bar, old completed tasks page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { useState, useEffect } from "react"
+import { Navbar, Nav } from 'react-bootstrap'
 import taskService from './services/tasks'
 import Home from './components/Home'
 import OldCompletedTasks from './components/OldCompletedTasks'
 
 const App = () => {
   const [ tasks, setTasks ] = useState([])
-
-  /*const now = new Date()
-  const dateSplit = now.toString().split(" ")
-  const dateToday = `${dateSplit[1]} ${dateSplit[2]}`*/
 
   useEffect(() => {
     taskService
@@ -19,12 +16,25 @@ const App = () => {
       })
   }, [])
 
+  const padding = {
+    padding: 5
+  }
+
   return(
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/oldcompleted">Old completed tasks</Link>
-      </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto" style={{marginLeft: "200"}}>
+            <Nav.Link href="/" as="span">
+              <Link style={padding} to="/">HOME</Link>
+            </Nav.Link>
+            <Nav.Link href="/oldcompleted" as="span">
+              <Link style={padding} to="/oldcompleted">OLD TASKS</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/oldcompleted" element={<OldCompletedTasks tasks={tasks} />} />
--- frontend/src/components/OldCompletedTasks.jsx
+++ frontend/src/components/OldCompletedTasks.jsx
@@ -1,17 +1,39 @@
 /* eslint-disable react/prop-types */
 
-const OldCompletedTasks = ({ tasks }) => {
-    const now = new Date()
-    const dateSplit = now.toString().split(" ")
-    const dateToday = `${dateSplit[1]} ${dateSplit[2]}`
-    
+import { Table } from "react-bootstrap"
+import TaskOld from "./TaskOld"
+
+const OldCompletedTasks = ({ tasks }) => {    
     return (
-        <div>OLD COMPLETED
-            <p>Tasks of earlier days:</p>
-            {tasks
-                .filter(task => task.done != false && task.dateCreated != dateToday)
-                .map(task => <li key={task.id}>{task.name} - {task.dateCreated} - {task.dateCompleted}</li>)}
-            <hr />
+        <div>
+            <br /><h2>OLD COMPLETED</h2> <hr />
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th align="center">Created</th>
+                        <th align="center">Completed</th>
+                        <th align="center">Priority</th>
+                        <th align="center">Number</th>
+                        <th align="center">Task</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {tasks
+                    .filter(task => task.done === true)
+                    .sort((a,b) => a.number < b.number ? 1 : -1)
+                    .sort((a,b) => a.priority > b.priority ? 1 : -1)
+                    .map(task =>
+                    <TaskOld
+                        key={task.id}
+                        name={task.name}
+                        priority={task.priority}
+                        number={task.number}
+                        dateCreated={task.dateCreated}
+                        dateCompleted={task.dateCompleted}
+                    />
+                )}
+                </tbody>
+        </Table><br />
         </div>
     )
 }
